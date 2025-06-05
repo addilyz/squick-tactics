53bc8bcedd72bc07a2f368f715ebe7745cf78f54
addilyz <addilyz@proton.me>
2025-06-04 21:54:56 -0700

    more setup, bit of progress

---
+++ project/index.lua
@@ -0,0 +1,11 @@
+local t = {}
+t.ignore = {
+	{"."},
+	{
+		"LICENSE","codex","tools","main.lua","README","bubble.lua",
+		"memmy.lua","conf.lua","slides.lua","nodes.lua","game",
+		"splashes","shredfx","cache"
+	}
+}
+t.window = {.7,.7}
+return t
--- runtime/grid.lua
+++ runtime/grid.lua
@@ -8,26 +8,28 @@ local fx = love.graphics
 local fs = love.filesystem
 local lt = love.thread
 fs.appendDirectory(true)
-if type(fs.getInfo("squick/tactics/grid/global")) == "table" then grid.settings = fs.load("squick/tactics/grid/global")() end
+if type(fs.getInfo("/project/grid/global")) == "table" then grid.settings = fs.load("project/grid/global")() end
 local threads = {}
-local rowThread = [[
-]]
 
-function grid.new(loc,canvas)
-	if type(canvas) == "table" then
-		field.hasTexture = true
-	end
+function grid.load(loc,canvas) -- canvas completely optional. surely if you want it you'll know. easy access.
+	loc = "grid/maps/" .. loc
 	local fieldObject = setmetatable(fs.load(loc)(),field)
+	if fieldObject.settings == nil then fieldObject.settings = grid.settings end
+	if type(canvas) ~= "nil" then
+		fieldObject.hasTexture = true
+		fieldObject.texture = canvas
+	else
+		fieldObject.hasTexture = false
+	end
 	return fieldObject
 end
 
-function field:preLoad()
-	if self.useProjectSettings then
-		self.settings = grid.settings
-	end
+function field:prepRender()
 	for y = 1, #self, 1 do
 		for x = 1, #self[y], 1 do
-			
+			if self[y][x] == true then
+				
+			end
 		end
 	end
 end
@@ -41,5 +43,7 @@ function field:preRenderAdjust(yaw,pitch)
 end
 
 function field:draw()
-	
+	for n = 1, #self.poly, 1 do
+		fx.polygon("line",self.poly[n])
+	end
 end
