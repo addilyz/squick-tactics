31471ce3aadfb11300d4bfbbfb064e55a98c9189
addilyn <addilyz@proton.me>
2025-06-25 17:05:34 -0700

    preparing recorder for grafficks

---
+++ test/conf.lua
@@ -0,0 +1,7 @@
+
+function love.conf(t)
+	t.window.width = 200
+	t.window.height = 200
+	t.identity = "rotworks"
+	t.window.vsync = 1
+end
---
+++ test/main.lua
@@ -0,0 +1,47 @@
+
+function love.load()
+	 local function recursivelyDelete( item )
+        if love.filesystem.getInfo( item , "directory" ) then
+            for _, child in ipairs( love.filesystem.getDirectoryItems( item )) do
+                recursivelyDelete( item .. '/' .. child )
+                love.filesystem.remove( item .. '/' .. child )
+            end
+        elseif love.filesystem.getInfo( item ) then
+            love.filesystem.remove( item )
+        end
+        love.filesystem.remove( item )
+    end
+    recursivelyDelete( 'recording' )
+	cv = love.graphics.newCanvas(40,40)
+	love.graphics.setCanvas(cv)
+	love.graphics.rectangle("fill",0,0,40,40)
+	love.graphics.setColor(1,.2,.2,1)
+	love.graphics.rectangle("fill",20,0,20,20)
+	love.graphics.setCanvas()
+	love.graphics.setColor(1,1,1,1)
+	rt = 0
+	love.filesystem.createDirectory("recording")
+end
+local frame = 0
+local dtc = 0
+function love.update(dt)
+	rt = rt + dt
+    dtc = dtc + dt
+    if dtc >= 1/30 then
+    	frame = frame+1
+    	local s = love.graphics.captureScreenshot(
+    	"recording/"..string.format("%012d",frame) ..".png" )
+	dtc = 0
+    end
+end
+local name = "recording.gif"
+function love.keypressed(k)
+	if k == "escape" then
+		os.execute( "ffmpeg -i " .. love.filesystem.getSaveDirectory() .. "/recording/%012d.png " .. name )
+		love.event.quit()
+	end
+end
+
+function love.draw()
+	love.graphics.draw(cv,60,60,rt,1,1,30,10)
+end
